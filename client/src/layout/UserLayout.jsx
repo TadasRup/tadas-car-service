e87532a43f2357a9d5a9b4e7316cc801501bb18e
Kevinas <129725556+TadasRup@users.noreply.github.com>
2023-09-19 13:56:48 +0300

    Masters Changes

--- client/src/layout/UserLayout.jsx
+++ client/src/layout/UserLayout.jsx
@@ -2,6 +2,7 @@ import {Footer} from "../components/Footer/Footer";
 import {Dashboard} from "../pages/Dashboard";
 import {DashboardHeader} from "../components/Header/DashboardHeader";
 import {DashboardPost} from "../pages/DashboardPost";
+import {DashboardPost2} from "../pages/DashboardPost2";
 
 
 export function UserLayout() {
@@ -10,6 +11,7 @@ export function UserLayout() {
             <DashboardHeader/>
             <main><Dashboard/></main>
             <DashboardPost />
+            <DashboardPost2 />
             <Footer/>
         </>
     )
